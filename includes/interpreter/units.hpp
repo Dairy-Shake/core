7c4fac6f5439085ac65d6f867b6ead29f5aeb437
flyhiawa <flyhiawa@gmail.com>
2026-08-22 02:29:14 +0000

    refactor: Identifiers in 'bytes_units.cpp' fixed

--- includes/interpreter/units.hpp
+++ includes/interpreter/units.hpp
@@ -1,7 +1,7 @@
-#ifndef UNITS_HPP
-#define UNITS_HPP
+#ifndef BYTES_UNITS_HPP
+#define BYTES_UNITS_HPP
 
-namespace Units {
+namespace Dairyshake::BytesUnits {
 	constexpr unsigned long long operator"" _kb(unsigned long long v) { return v * 1024; };
 	constexpr unsigned long long operator"" _mb(unsigned long long v) { return v * 1_kb; };
 	constexpr unsigned long long operator"" _gb(unsigned long long v) { return v * 1_mb; };
